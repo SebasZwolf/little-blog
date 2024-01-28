bd3183c8d0e62c446b9721666faff6c6267f591d
S.R <abel20100311@gmail.com>
2024-01-28 03:13:51 -0500

    fix!

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://SebasZwolf.github.io',
-	base : 'little-blog',
+	base : '/little-blog',
 	integrations: [
 		sitemap()
 	],
