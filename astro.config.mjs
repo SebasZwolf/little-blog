9660b6629dc89baed44ecab6417faecd7e42f1a2
S.R <abel20100311@gmail.com>
2024-03-21 13:38:40 -0500

    chore: route+links config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	site: process.env.URL ?? 'https://SebasZwolf.github.io',
-	base : process.env.BASE ?? '/',
+	site : process.env.URL ?? 'https://SebasZwolf.github.io',
+	base : process.env.BASE,
 	integrations: [
 		sitemap()
 	],
